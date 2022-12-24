f12deeddeaa0a0d9a3936fb0ecf255df6e534e6f
Александр Борисов <alexandrborisov88@mail.ru>
2022-12-24 23:03:36 +0300

    finish func

--- DSPR 2.0/Блок 0/python 6/2_7/2_7/_2_7.py
+++ DSPR 2.0/Блок 0/python 6/2_7/2_7/_2_7.py
@@ -3,4 +3,9 @@
 
 def holes_count(number):
     holes_dict = {'0':1, '1':0, '2':0, '3':0, '4':1, '5':0, '6':1, '7':0, '8':2, '9':1}
+    str_num = str(number)
+    col_holes = 0
+    for i in str_num:
+        col_holes += holes_dict[i]
+    return col_holes
 
